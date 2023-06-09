3fea00c98bf10a1b5ee0efd1cc0c08fefc405742
Sameer Channar <schannar@asu.edu>
2023-06-08 17:22:33 -0700

    added lives

---
+++ Assets/Scripts/LivesDisplay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LivesDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+        textComponent.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textComponent.text = "Lives: " + global.lives.ToString();
+    }
+}
